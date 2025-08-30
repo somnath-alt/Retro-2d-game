aa69e1458fbb38816ed30ac1647b7d2df44be583
Somnath-alt <soumyo.saha04@gmail.com>
2025-08-31 02:35:08 +0530

    change

--- alien_invation.py
+++ alien_invation.py
@@ -15,7 +15,7 @@ class AlienInvasion:
         self.settings = Settings()  # Creating an instance of Settings
         
         # setting up the screen
-        self.screen = pygame.display.set_mode((self.settings.screen_widyh, self.settings.screen_height)) 
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)  # Creating an instance of Ship
@@ -28,18 +28,28 @@ class AlienInvasion:
     def run_game(self):
         """Strating the main loop for the game"""
         while True:
-            # Watch for keyboard and Mouse Events 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)  # Fill the screen with the background color        
-            
-            self.ship.blitme()
-            # Make the most recently drawn screen Visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60) # Limit the game to 60 frames per second
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    ##Moving the ship to the right 
+                    self.ship.rect.x += 1    
+
+    def _update_screen(self):
+        """Update image on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)  # Fill the screen with the background color        
+        self.ship.blitme()
+        pygame.display.flip()
+            
+            
 
 if __name__ == '__main__':
     # Make a game instance, and Run the game
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen Settings
-        self.screen_widyh = 1523
+        self.screen_width = 1523
         self.screen_height = 765
         self.bg_color = (230, 230, 230)
         
