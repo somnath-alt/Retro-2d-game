2775f2c8f860a2637cf631d0afd13b5b14e2abbb
Somnath Saha <soumyo.saha04@gmail.com>
2026-05-19 01:15:32 +0530

    Added shooting

--- alien_invation.py
+++ alien_invation.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
     def __init__(self):
@@ -13,16 +15,20 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()  # Creating a clock object 
         self.settings = Settings()  # Creating an instance of Settings
-        
+        #Group to store bullets in
+        self.bullets = pygame.sprite.Group()
+
         # setting up the screen
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height 
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)  # Creating an instance of Ship
 
         # seting Background colour
 
-        self.bg_color = (230, 230, 230) # RGB color for light gray
+        self.bg_color = (0, 230, 230) # RGB color for light gray
 
 
     def run_game(self):
@@ -31,6 +37,12 @@ class AlienInvasion:
 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            #Deleting the bullets which have passed the screen
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <=0:
+                    self.bullets.remove(bullet)
             self._update_screen()
             self.clock.tick(60) # Limit the game to 60 frames per second
 
@@ -41,21 +53,40 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 ##Moving the ship to the right when the right arrow key is pressed
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False           
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False    
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()  
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()      
+
+    def _check_keyup_events(self,event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = False           
+        elif event.key == pygame.K_LEFT:
+             self.ship.moving_left = False    
+
+    def _fire_bullet(self):
+        """Createing a new bullet and add it to the bullets group""" 
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)               
 
     def _update_screen(self):
         """Update image on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)  # Fill the screen with the background color        
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
             
             
--- settings.py
+++ settings.py
@@ -7,4 +7,9 @@ class Settings:
         self.screen_width = 1523
         self.screen_height = 765
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 2.5
+        self.bullet_speed = 3.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
         
